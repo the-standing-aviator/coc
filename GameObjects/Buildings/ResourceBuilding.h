ad88cd40c3e7b76f61bcdb96cf4e1bf3e2a7ef59
Leoyyc1121 <2835193356@qq.com>
2025-12-12 12:37:23 +0800

    Delete GameObjects directory

--- GameObjects/Buildings/ResourceBuilding.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "GameObjects/Buildings/Building.h"
-
-class ElixirCollector : public Building { public: ElixirCollector() { image = "buildings/buildings3.png"; } };
-class ElixirStorage : public Building { public: ElixirStorage() { image = "buildings/buildings4.png"; } };
-class GoldMine : public Building { public: GoldMine() { image = "buildings/buildings5.png"; } };
-class GoldStorage : public Building { public: GoldStorage() { image = "buildings/buildings6.png"; } };
